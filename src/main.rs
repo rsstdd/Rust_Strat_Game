7ab2c26687e17a72894d956246ff8fcadec5dfcb
rsstdd <rosstod@gmail.com>
2017-08-30 22:56:03 -0700

    Add Test"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_empty_grid() {
+        let grid = ::Grid::generate_empty(5, 13); // Generate empty grid (5x13)
+        assert_eq!(grid.size, (5, 13)); // Assert that new grid gets its size set accordingly
+        let mut number_of_squares = 0; 
+        
+        for square in &grid.squares { // checks that each square is not a being or block
+            assert_eq!(square.ground, ::TerrainGround::Soil);
+            assert_eq!(square.block, None);
+            assert_eq!(square.beings, None);
+            number_of_squares += 1; // Increments the counter
+        }
+        
+        assert_eq!(grid.squares.len(), 5*13); // Should have this many
+        assert_eq!(number_of_squares, 5*13); // Actually has this many
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum TerrainGround {
+    Soil, 
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum TerrainBlock {
+    Tree, 
+    Soil, 
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum Being {
+    Orc,
+    Human
+}
+
+struct Square {
+    ground: TerrainGround,
+    block: Option<TerrainBlock>, // "This is a thing of Type T that might not exist"
+    beings: Option<Being>
+}
+
+struct Grid {
+    size: (usize, usize),
+    squares: Vec<Square> // Vector<T> means a vector of things type T (In this case, a Generic)
+}
+
+impl Grid {
+    fn generate_empty(size_x: usize, size_y: usize) -> Grid { // returns a grid
+        let number_of_squares = size_x * size_y;
+        let mut squares: Vec<Square> = Vec::with_capacity(number_of_squares);
+        
+        for _ in 1..number_of_squares { // compute # of squares in grid
+            squares.push(Square{ground: TerrainGround::Stone, block: None, beings: None});
+        }
+        
+        Grid {
+            size: (size_x, size_y),
+            squares: squares
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
